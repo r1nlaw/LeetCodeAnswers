8a97a0cce7e3eb88e79c093c108dfd81c01f16a9
r1nlaw <gru.zhizhi@mail.ru>
2025-02-05 22:05:06 +0300

    Add new answers for Easy

---
+++ Easy/SameTree/main.go
@@ -0,0 +1,27 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Right *TreeNode
+	Left  *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+
+}
+
+func main() {
+
+}
